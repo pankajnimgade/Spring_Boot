79eb48d393ea0d87827c20d21132dd252674fe1d
Pankaj Nimgade <pankaj.nimgade@gmail.com>
2018-02-04 21:56:36 -0600

    added a module# Test101

---
+++ Test101/src/main/java/com/test101/rest/api/test101/rest/api/Test101Application.java
@@ -0,0 +1,12 @@
+package com.test101.rest.api.test101.rest.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test101Application.class, args);
+	}
+}
