52d77c2a9827ef73a1dae0b49eeb950dc9778520
Pankaj Nimgade <pankaj.nimgade@gmail.com>
2018-02-06 15:14:07 -0600

    External Database module added

---
+++ ExternalDatabase/src/main/java/com/external/db/jpa/external/db/ExternalDataBaseApplication.java
@@ -0,0 +1,12 @@
+package com.external.db.jpa.external.db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalDataBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExternalDataBaseApplication.class, args);
+	}
+}
---
+++ ExternalDatabase/src/test/java/com/external/db/jpa/external/db/ExternalDataBaseApplicationTests.java
@@ -0,0 +1,16 @@
+package com.external.db.jpa.external.db;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExternalDataBaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
