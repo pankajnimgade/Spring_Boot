dbee79be9f096d423fd6a9b9928a38dbc5fa0481
Pankaj Nimgade <pankaj.nimgade@gmail.com>
2018-02-06 16:46:43 -0600

    contact controller and service done and checked

--- ExternalDatabase/src/main/java/com/external/db/jpa/external/db/contact/ContactController.java
+++ ExternalDatabase/src/main/java/com/external/db/jpa/external/db/contact/ContactController.java
@@ -1,4 +1,38 @@
 package com.external.db.jpa.external.db.contact;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class ContactController {
+
+    @Autowired
+    private ContactService contactService;
+
+    @RequestMapping(value = "/contacts", method = RequestMethod.GET)
+    public List<Contact> getAllContacts() {
+        return contactService.getAllContacts();
+    }
+
+    @RequestMapping(value = "/contacts/{phoneNumber}", method = RequestMethod.GET)
+    public Contact getAllContacts(@PathVariable String phoneNumber) {
+        return contactService.getContact(phoneNumber);
+    }
+
+    @RequestMapping(value = "/contacts", method = RequestMethod.POST)
+    public Contact addContact(@RequestBody Contact contact){
+        return contactService.addContact(contact);
+    }
+
+    @RequestMapping(value = "/contacts/{phoneNumber}", method = RequestMethod.PUT)
+    public Contact updateContact(@PathVariable String phoneNumber, @RequestBody Contact contact){
+        return contactService.updateContact(contact);
+    }
+
+    @RequestMapping(value = "/contacts/{phoneNumber}", method = RequestMethod.DELETE)
+    public boolean deleteContact(@PathVariable String phoneNumber){
+        return contactService.deleteContact(phoneNumber);
+    }
 }
--- ExternalDatabase/src/main/java/com/external/db/jpa/external/db/contact/ContactService.java
+++ ExternalDatabase/src/main/java/com/external/db/jpa/external/db/contact/ContactService.java
@@ -1,4 +1,40 @@
 package com.external.db.jpa.external.db.contact;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class ContactService {
+
+    @Autowired
+    private ContactRepository contactRepository;
+
+    public List<Contact> getAllContacts() {
+        List<Contact> contactList = new ArrayList<>();
+        contactRepository.findAll().forEach(contact -> contactList.add(contact));
+        return contactList;
+    }
+
+    public Contact getContact(String phoneNumber) {
+        return contactRepository.findOne(phoneNumber);
+    }
+
+    public Contact addContact(Contact contact) {
+        return contactRepository.save(contact);
+    }
+
+    public Contact updateContact(Contact contact) {
+        return contactRepository.save(contact);
+    }
+
+    public boolean deleteContact(String phoneNumber) {
+        if (contactRepository.exists(phoneNumber)) {
+            contactRepository.delete(phoneNumber);
+            return true;
+        }
+        return false;
+    }
 }
